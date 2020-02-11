cbf38ca4195ec237ebb2bd824f6651e8e914b2bc
jnnnn132 <jiraput_si@cmu.ac.th>
2020-02-11 09:54:36 +0700

    testpush

---
+++ app.js
@@ -0,0 +1,14 @@
+const {Person} = require('./Person');
+console.log("ddddd");
+const person = {
+    name: 'Jiraphat Singnoi',
+    age: 20
+}
+console.log(person.name+',age:'+person.age);
+console.log(person);
+
+
+
+
+const p1 = new Person('Jane',20);
+p1.greeting();
